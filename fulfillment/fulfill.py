18ca46b20879391d55b8ecaf37a1bd2b4c8bb50c
alwaysdelivers <hello@alwaysdelivers.com>
2026-06-17 02:32:47 -0700

    fulfill: yeti tee placement validated

--- fulfillment/fulfill.py
+++ fulfillment/fulfill.py
@@ -67,7 +67,7 @@ def line_to_item(title,color,size,qty=1,retail=None,print_style=None):
         fp='printfiles/'+code
         aw,ah=AREA[garment]
         if garment=='hoodie': w,h,top=1637,2100,0        # MAX front (10.9" x 14")
-        else: w=1500; h=int(w/0.7796); top=180            # tee: validate before launch
+        else: w,h,top=1600,int(1600/0.7796),110           # tee: validated on Men's 4
         return {'variant_id':cv,'quantity':qty,'retail_price':retail,
                 'files':[{'type':'front','url':RAW+fp,'position':{'area_width':aw,'area_height':ah,'width':w,'height':h,'top':top,'left':(aw-w)//2}}],
                 '_design':'yeti','_garment':garment,'_file':fp,'_flags':[]}
