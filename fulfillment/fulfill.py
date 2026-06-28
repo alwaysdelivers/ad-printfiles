c247610fda013d17037a5bb6d8826441e063f759
alwaysdelivers <hello@alwaysdelivers.com>
2026-06-28 14:36:31 -0700

    Add gold to AMERICA Navy/WM dark garments; watermark_gold_dark routing

--- fulfillment/fulfill.py
+++ fulfillment/fulfill.py
@@ -37,8 +37,8 @@ GOD_VALID={'white':['navy','split','red','black','karmablue'],'athleticheather':
 GOD_DEFAULT={'white':'navy','athleticheather':'navy','navy':'cream','black':'cream'}
 AMERICA_STYLES={'classic':('classic',2010/1363),'heritage':('heritage',2010/1674),'star':('star',2010/1921),'retro':('retro',2010/1160),'watermark':('watermark',2010/1315)}
 AMERICA_INK={'navy':'navy','red':'red','black':'black','gold':'gold','white':'white','grey':'grey','heather grey':'grey','heathergrey':'grey','karmablue':'karmablue','karma blue':'karmablue','ice':'ice','ice blue':'ice','iceblue':'ice','karma':'karma'}
-AMERICA_VALID_STD={'white':['navy','red','black','gold','karmablue'],'athleticheather':['navy','red','black','gold','karmablue'],'navy':['white','grey','ice','red'],'black':['red','white','grey','karmablue','ice']}
-AMERICA_VALID_WM={'white':['navy','red'],'athleticheather':['navy','red'],'navy':['red'],'black':['ice','karma','red']}
+AMERICA_VALID_STD={'white':['navy','red','black','gold','karmablue'],'athleticheather':['navy','red','black','gold','karmablue'],'navy':['white','grey','ice','red','gold'],'black':['red','white','grey','karmablue','ice']}
+AMERICA_VALID_WM={'white':['navy','red'],'athleticheather':['navy','red'],'navy':['red','gold'],'black':['ice','karma','red','gold']}
 AMERICA_DEFAULT_STD={'white':'navy','athleticheather':'navy','navy':'white','black':'red'}
 AMERICA_DEFAULT_WM={'white':'navy','athleticheather':'navy','navy':'navy','black':'ice'}
 AMERICA_MAXW=1600; AMERICA_TOP={'tee':480,'hoodie':470}
@@ -265,6 +265,8 @@ def line_to_item(title,color,size,qty=1,retail=None,print_style=None,ink=None):
         if not tk or tk not in valid: tk=default
         if st=='watermark' and tk=='red' and ckey not in ('white','athleticheather','sportgrey'):
             fp='printfiles/america/watermark_red_dark.png'
+        elif st=='watermark' and tk=='gold' and ckey not in ('white','athleticheather','sportgrey'):
+            fp='printfiles/america/watermark_gold_dark.png'
         else:
             fp='printfiles/america/%s_%s.png'%(code,tk)
         aw,ah=AREA[garment]; w=AMERICA_MAXW; h=int(w/aspect); top=AMERICA_TOP[garment]; left=(aw-w)//2
